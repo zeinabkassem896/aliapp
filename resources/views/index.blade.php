10ef8975303f32fb1384fc965b2ba860887086f6
zeinabkassem896 <zeinabkassem896@gmail.com>
2022-08-22 18:00:40 +0300

    sidebar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('nav')
+
+@section('content')
+  
+  <div class="col p-4">
+        <h1 class="display-4">Collapsing Sidebar Menu</h1>
+        <div class="card">
+            <h5 class="card-header font-weight-light">Requirements</h5>
+            <div class="card-body">
+                <ul>
+                    <li>JQuery</li>
+                    <li>Bootstrap 4.3</li>
+                    <li>FontAwesome</li>
+                </ul>
+            </div>
+        </div>
+    </div><!-- Main Col END -->
+
+    @endsection
